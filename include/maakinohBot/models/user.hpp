29083715d22f0520c600cdd7ba49decf8426d541
maakinoh <maakinoh@gmail.com>
2019-09-17 21:32:03 +0200

    .

--- include/maakinohBot/models/user.hpp
+++ include/maakinohBot/models/user.hpp
@@ -1,9 +1,15 @@
 #pragma once
 
 #include <maakinohBot/models/parseable_object.hpp>
+#include <nlohmann/json.hpp>
 
-namespace MaakinohBot::Models{
-    class User : public ParseableObject{
+using json = nlohmann::json;
+namespace MaakinohBot::Models
+{
+
+
+    class User : public ParseableObject
+    {
     public:
         int id;
         bool is_bot;
@@ -14,4 +20,22 @@ namespace MaakinohBot::Models{
 
         void parse(std::string jsonString) override;
     };
+
+    void to_json(json &j, const User &p)
+    {
+        j = json{{"id",            p.id},
+                 {"is_bot",        p.is_bot},
+                 {"first_name",    p.first_name},
+                 {"last_name",     p.last_name},
+                 {"username",      p.username},
+                 {"language_code", p.language_code}};
+    }
+
+
+    void from_json(const json& j, User& p) {
+        j.at("name").get_to(p.name);
+        j.at("address").get_to(p.address);
+        j.at("age").get_to(p.age);
+    }
+
 }
--- src/maakinohBot/models/user.cpp
+++ src/maakinohBot/models/user.cpp
@@ -1,14 +1,13 @@
 #include <maakinohBot/models/user.hpp>
-
 #include <nlohmann/json.hpp>
+
+
 
 void MaakinohBot::Models::User::parse(std::string jsonString)
 {
     ParseableObject::parse(jsonString);
     auto j = nlohmann::json::parse(jsonString);
 
-    this->id = j["id"].get<int>();
-    this->is_bot
-
+    auto a = j.get<Models::User>();
 
 }
